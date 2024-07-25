d10b3c5d9aa410ad3f764c0e5d2a94294ac237fd
jhonatanmizu <natanjesuss20@gmail.com>
2024-07-24 22:49:09 -0300

    refactor: remove module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,31 +6,7 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    DatabaseModule,
-    UsersModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'mariadb',
-    //   host: process.env.DATABASE_HOST,
-    //   port: Number(process.env.DATABASE_PORT),
-    //   username: process.env.DATABASE_USER,
-    //   password: process.env.DATABASE_PASSWORD,
-    //   database: process.env.DATABASE_NAME,
-    //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'mariadb',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'my-secret',
-    //   database: 'mydb',
-    //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
-  ],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
