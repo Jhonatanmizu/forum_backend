f1e67bc9c9f41f07063cac20203ff1d8b8262e60
jhonatanmizu <natanjesuss20@gmail.com>
2024-07-24 22:47:20 -0300

    feat: create user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { UserRole } from '../enums';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 156 })
+  name: string;
+
+  @Column('uuid', { generated: 'uuid', unique: true })
+  subscription: string;
+
+  @Column('varchar', { length: 20 })
+  identity: string;
+
+  @Column('date')
+  birthdate: Date;
+
+  @Column('varchar', { length: 20 })
+  phone: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.PARTICIPANT,
+  })
+  role: UserRole;
+}
