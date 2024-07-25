952bbdf702e598d9f6c40c6a0f86f86ed3f541f0
jhonatanmizu <natanjesuss20@gmail.com>
2024-07-24 22:48:40 -0300

    feat: building migrations

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,13 +1,26 @@
+import { registerAs } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 export const DATA_SOURCE_PROVIDER = 'DATA_SOURCE';
 
 const SYNCHRONIZE = process.env.ENVIRONMENT !== 'production';
 
+export const typeormDataSource = new DataSource({
+  type: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  synchronize: SYNCHRONIZE,
+});
+
 export const databaseProviders = [
   {
     provide: DATA_SOURCE_PROVIDER,
-    useFactory: async () => {
+    useFactory: () => {
       const dataSource = new DataSource({
         type: 'mariadb',
         host: process.env.DATABASE_HOST,
@@ -16,6 +29,7 @@ export const databaseProviders = [
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: ['dist/migrations/*{.ts,.js}'],
         synchronize: SYNCHRONIZE,
       });
 
@@ -23,3 +37,5 @@ export const databaseProviders = [
     },
   },
 ];
+
+export default registerAs('typeorm', () => typeormDataSource);
